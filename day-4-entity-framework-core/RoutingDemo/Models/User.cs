2a6b3ccb905931bcfc41812ac7d7b48094749d54
Maina.dev <138670482+Mainaaa1@users.noreply.github.com>
2026-02-19 15:52:05 +0300

    updated user model

--- day-4-entity-framework-core/RoutingDemo/Models/User.cs
+++ day-4-entity-framework-core/RoutingDemo/Models/User.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RoutingDemo.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
+
         public bool IsActive { get; set; }
     }
 }
