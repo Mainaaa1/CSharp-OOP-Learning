8e1390d5d66096bbdeec5df4a26316fc3aac74a5
Maina.dev <138670482+Mainaaa1@users.noreply.github.com>
2026-02-19 15:55:31 +0300

    Updated Program.cs to register DbContext

--- day-4-entity-framework-core/RoutingDemo/Program.cs
+++ day-4-entity-framework-core/RoutingDemo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using RoutingDemo.Data;
 using RoutingDemo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register DbContext (SQLite)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
 
 // Register Service
 builder.Services.AddScoped<IUserService, UserService>();
